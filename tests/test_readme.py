111e704491848a18963b9da9dfc47f062a025296
Talley Lambert <talley.lambert@gmail.com>
2021-10-07 11:24:52 -0400

    skip on windows

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,9 +1,13 @@
+import os
 from pathlib import Path
+
+import pytest
 
 README = Path(__file__).parent.parent / "README.md"
 SAMPLE = Path(__file__).parent / "data" / "dims_c2y32x32.nd2"
 
 
+@pytest.mark.skipif(os.name == "nt")
 def test_readme(capsys):
     code = README.read_text().split("```python")[1].split("```")[0]
     code = code.replace("some_file.nd2", str(SAMPLE.absolute()))

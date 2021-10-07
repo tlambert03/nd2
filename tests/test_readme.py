c2925083c2696e48edfc6b09d43d74ad8d62fa0d
Talley Lambert <talley.lambert@gmail.com>
2021-10-07 11:58:07 -0400

    fix

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -7,7 +7,7 @@ README = Path(__file__).parent.parent / "README.md"
 SAMPLE = Path(__file__).parent / "data" / "dims_c2y32x32.nd2"
 
 
-@pytest.mark.skipif(os.name == "nt")
+@pytest.mark.skipif(os.name == "nt", reason="paths annoying on windows")
 def test_readme(capsys):
     code = README.read_text().split("```python")[1].split("```")[0]
     code = code.replace("some_file.nd2", str(SAMPLE.absolute()))

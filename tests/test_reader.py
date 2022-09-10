ef9161aeddeaced323d03dc0992729cf8bd65530
Talley Lambert <talley.lambert@gmail.com>
2022-09-10 19:52:40 -0400

    fix: fix image width again (#92)

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,7 +2,6 @@ import json
 import os
 import pickle
 import sys
-from contextlib import nullcontext
 from pathlib import Path
 
 import dask.array as da
@@ -17,30 +16,14 @@ from nd2._util import AXIS
 DATA = Path(__file__).parent / "data"
 
 
-def _warning_ctx(fname: Path):
-    if fname.name in {
-        "jonas_control002.nd2",
-        "jonas_JJ1473_control_24h_JJ1473_control_24h_03.nd2",
-    }:
-        return pytest.warns(UserWarning, match="widthPx")
-    return nullcontext()
-
-
 def test_metadata_extraction(new_nd2: Path):
     assert ND2File.is_supported_file(new_nd2)
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         assert nd.path == str(new_nd2)
         assert not nd.closed
 
         # assert isinstance(nd._rdr._seq_count(), int)
         assert isinstance(nd.attributes, structures.Attributes)
-
-        # this is one of the "skewed" files where widthPx seems
-        # to be set incorrectly in the actual metadata
-        if new_nd2.name == "jonas_control002.nd2":
-            assert nd.attributes.widthPx == 248
-            assert nd.shape == (65, 9, 152, 248)
-            assert nd.sizes["X"] == 248
 
         # TODO: deal with typing when metadata is completely missing
         assert isinstance(nd.metadata, structures.Metadata)
@@ -58,7 +41,7 @@ def test_metadata_extraction(new_nd2: Path):
 
 
 def test_read_safety(new_nd2: Path):
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         for i in range(nd._frame_count):
             nd._rdr._read_image(i)
 
@@ -67,7 +50,7 @@ def test_position(new_nd2):
     """use position to extract a single stage position with asarray."""
     if new_nd2.stat().st_size > 250_000_000:
         pytest.skip("skipping read on big files")
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         dx = nd.to_xarray(delayed=True, position=0, squeeze=False)
         nx = nd.to_xarray(delayed=False, position=0, squeeze=False)
         assert dx.sizes[AXIS.POSITION] == 1
@@ -79,7 +62,7 @@ def test_position(new_nd2):
 
 
 def test_dask(new_nd2):
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         dsk = nd.to_dask()
         assert isinstance(dsk, da.Array)
         assert dsk.shape == nd.shape
@@ -96,7 +79,7 @@ def test_dask_closed(single_nd2):
 
 @pytest.mark.skipif(bool(os.getenv("CIBUILDWHEEL")), reason="slow")
 def test_full_read(new_nd2):
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         if new_nd2.stat().st_size > 500_000_000:
             pytest.skip("skipping full read on big files")
         delayed_xarray: np.ndarray = np.asarray(nd.to_xarray(delayed=True))
@@ -126,7 +109,7 @@ def test_full_read_legacy(old_nd2):
 
 
 def test_xarray(new_nd2):
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         xarr = nd.to_xarray()
         assert isinstance(xarr, xr.DataArray)
         assert isinstance(xarr.data, da.Array)
@@ -193,7 +176,7 @@ def test_bioformats_parity(new_nd2: Path, bfshapes: dict):
         bf_info = {k: v for k, v in bfshapes[new_nd2.name]["shape"].items() if v > 1}
     except KeyError:
         pytest.skip(f"{new_nd2.name} not in stats")
-    with _warning_ctx(new_nd2), ND2File(new_nd2) as nd:
+    with ND2File(new_nd2) as nd:
         # doing these weird checks/asserts for better error messages
         if len(bf_info) != len(nd.sizes):
             assert bf_info == nd.sizes
@@ -304,3 +287,22 @@ def test_with_without_sdk(small_nd2s: Path):
             ValueError, match="compressed nd2 files with `read_using_sdk=False`"
         ):
             imread(small_nd2s, read_using_sdk=False)
+
+
+def test_extra_width_bytes():
+    expected = [
+        [203, 195, 193, 197],
+        [203, 195, 195, 197],
+        [205, 191, 192, 190],
+        [204, 201, 196, 206],
+    ]
+
+    im = imread(str(DATA / "jonas_JJ1473_control_24h_JJ1473_control_24h_03.nd2"))
+    np.testing.assert_array_equal(im[0, 0, :4, :4], expected)
+
+    im = imread(
+        str(DATA / "jonas_JJ1473_control_24h_JJ1473_control_24h_03.nd2"),
+        read_using_sdk=True,
+    )
+    # NOTE: we actually WANT this to pass... but the SDK isn't doing the right thing
+    assert not np.array_equal(im[0, 0, :4, :4], expected)

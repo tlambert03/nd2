1c8cc4172f93b79b5b31028576e925cd033956e9
Talley Lambert <talley.lambert@gmail.com>
2023-06-12 11:57:01 -0400

    add consistency test

---
+++ tests/test_metadata.py
@@ -0,0 +1,34 @@
+import json
+import sys
+from pathlib import Path
+
+import pytest
+
+sys.path.append(str(Path(__file__).parent.parent / "scripts"))
+from nd2_describe import get_nd2_stats  # noqa: E402
+
+with open("tests/samples_metadata.json") as f:
+    EXPECTED = json.load(f)
+
+
+@pytest.mark.parametrize("path", EXPECTED, ids=lambda x: f'{x}_{EXPECTED[x]["ver"]}')
+def test_metadata_integrity(path: str):
+    target = Path("tests/data") / path
+    name, stats = get_nd2_stats(target)
+
+    # normalize serizalized stuff
+    stats = json.loads(json.dumps(stats, default=str))
+
+    for key in stats:
+        # The SDK has a bug in position name fetching... we do it better, so just clear
+        if key == "experiment" and stats["ver"] >= "Ver3.0":
+            _clear_names(stats[key], EXPECTED[name][key])
+        assert stats[key] == EXPECTED[name][key], f"{key} mismatch"
+
+
+def _clear_names(*exps: dict | list):
+    for exp in exps:
+        for item in exp:
+            if item["type"] == "XYPosLoop":
+                for point in item["parameters"]["points"]:
+                    point.pop("name", None)

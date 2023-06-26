a39fa131052289ae76f035358e564c8480a59c35
Talley Lambert <talley.lambert@gmail.com>
2023-06-26 12:24:45 -0400

    add more files

--- tests/test_codspeed.py
+++ tests/test_codspeed.py
@@ -7,20 +7,27 @@ import pytest
 if all(x not in {"--codspeed", "tests/test_codspeed.py"} for x in sys.argv):
     pytest.skip("use --codspeed to run benchmarks", allow_module_level=True)
 
-
+# fmt: off
 DATA = Path(__file__).parent / "data"
-TEST_FILE = DATA / "train_TR67_Inj7_fr50.nd2"
+FILES: list[Path] = [
+    DATA / "aryeh_but3_cont200-1.nd2",      # v1.0 -  16.14 MB
+    DATA / "jonas_control002.nd2",          # v2.1 -  48.05 MB
+    DATA / "dims_p2z5t3-2c4y32x32.nd2",     # v3.0 -   1.10 MB
+    DATA / "train_TR67_Inj7_fr50.nd2",      # v3.0 -  69.55 MB
+    DATA / "karl_sample_image.nd2",         # v3.0 - 218.65 MB
+]
+# fmt: on
 
 
 @pytest.mark.benchmark
-@pytest.mark.parametrize("file", [TEST_FILE], ids=lambda x: x.stem)
+@pytest.mark.parametrize("file", FILES, ids=lambda x: x.stem)
 def test_time_imread(file: Path) -> None:
     """Test time to read a file."""
     _ = nd2.imread(file)
 
 
 @pytest.mark.benchmark
-@pytest.mark.parametrize("file", [TEST_FILE], ids=lambda x: x.stem)
+@pytest.mark.parametrize("file", FILES, ids=lambda x: x.stem)
 def test_time_all_metadata(file: Path) -> None:
     """Test time to read all metadata."""
     with nd2.ND2File(file) as nd:

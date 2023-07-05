32b4fe0ba20e5e5b63c85413b06d4cfd17dd72d2
Talley Lambert <talley.lambert@gmail.com>
2023-07-05 19:19:55 -0400

    fix: fix extra kwargs to roi info

--- src/nd2/structures.py
+++ src/nd2/structures.py
@@ -450,10 +450,22 @@ class ROI:
         # 'Id', 'Info', 'GUID', 'AnimParams_Size', 'AnimParams_{i}'
         # where GUID and AnimParams keys are optional
         anim_params = [
-            AnimParam(**{_lower0(k): v for k, v in val[f"AnimParams_{i}"].items()})
+            AnimParam(
+                **{
+                    _lower0(k): v
+                    for k, v in val[f"AnimParams_{i}"].items()
+                    if _lower0(k) in AnimParam.__annotations__
+                }
+            )
             for i in range(val.get("AnimParams_Size", 0))
         ]
-        info = RoiInfo(**{_lower0(k): v for k, v in val["Info"].items()})
+        info = RoiInfo(
+            **{
+                _lower0(k): v
+                for k, v in val["Info"].items()
+                if _lower0(k) in RoiInfo.__annotations__
+            }
+        )
         return cls(
             id=val["Id"],
             info=info,

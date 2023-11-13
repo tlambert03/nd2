f1d4fcfc7fd6f15fad61aa00d8a87d6185347784
Talley Lambert <talley.lambert@gmail.com>
2023-11-13 14:43:47 -0500

    whoops 38

--- tests/test_aicsimage.py
+++ tests/test_aicsimage.py
@@ -16,7 +16,7 @@ from aicsimageio.readers.nd2_reader import ND2Reader  # noqa
 
 DATA = Path(__file__).parent / "data"
 
-@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python3.9 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher")
 @pytest.mark.parametrize(
     (
         "filename",

4f9af367a304aa67fe4e2cde82881b5c46cd0144
Talley Lambert <talley.lambert@gmail.com>
2023-11-14 15:02:16 -0500

    remove

--- x.py
+++
@@ -1,9 +0,0 @@
-
-import nd2
-
-with nd2.ND2File("/Users/talley/Downloads/2023-09-11_MLY003_Cpf1.nd2") as f:
-    print(f.experiment)
-    print(f.attributes)
-    # print(f._rdr._raw_experiment)
-    print(f.read_frame(1000))
-    breakpoint()

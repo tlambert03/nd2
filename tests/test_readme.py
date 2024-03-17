3f21dc7287595aedbc124e8e97ffdcdd5eec649c
Talley Lambert <talley.lambert@gmail.com>
2024-03-17 13:27:06 -0400

    fix readme test

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -8,11 +8,12 @@ SAMPLE = Path(__file__).parent / "data" / "dims_c2y32x32.nd2"
 
 
 @pytest.mark.skipif(os.name == "nt", reason="paths annoying on windows")
-def test_readme(capsys):
+def test_readme(capsys: pytest.CaptureFixture, tmp_path: Path) -> None:
     pytest.importorskip("xarray")
 
     code = README.read_text().split("```python")[1].split("```")[0]
     code = code.replace("some_file.nd2", str(SAMPLE.absolute()))
+    code = code.replace("output.ome.tif", str(tmp_path / "output.ome.tif"))
     exec(code)
     captured = capsys.readouterr()
     assert captured.out.startswith("Metadata(")  # because of the print statement
